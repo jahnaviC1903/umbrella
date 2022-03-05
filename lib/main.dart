413a07ba705cd6ec897488ec9127f52c3b9daad5
jahnavi chandrakar <chandraker.jahnavi@gmail.com>
2022-03-05 14:08:40 +0300

    added icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,87 @@
-
-
+import 'package:custom_switch/custom_switch.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+void main() => runApp(MyApp());
 
-void main() => runApp(MaterialApp(
-    
-  home: Home()
-
-  ));
-  
-  class Home extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.yellow[50],
-         
-    appBar: AppBar(
-      title: Text('Major project'),
-      centerTitle: true,
-      backgroundColor: Colors.green,
-    ),
-    body: Center(
-      child:Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.min,
-      children:[ SizedBox(
-  width: 200.0,
-  height: 300.0,
-  //child: Card(child: Text('hi')),
-
-child:ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  
-                  primary: Colors.green[500], //background color of button
-                  //side: BorderSide(width:3, color:Colors.cyan[100]), //border width and color
-                  elevation: 3, //elevation of button
-                  shape: RoundedRectangleBorder( //to set border radius to button
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  padding: EdgeInsets.all(20) //content padding inside button
-                
-              ),
-                 onPressed: (){ 
-                   
-                }, 
-                child: Text("flap") 
-              )
-            ),]
-      //child: Image(
-        //image: NetworkImage('https://unsplash.com/photos/LmyPLbbUWhA'),)
-    
-   /* floatingActionButton: FloatingActionButton(onPressed: (){},
-      child: Text('click'),
-      ),*/
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
       ),
-  ),);
-    }
+      home: HomeScreen(),
+    );
   }
-  
+}
 
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  bool status = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom Switch Example'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+              Icon(
+      CupertinoIcons.bell,
+      color: Colors.green,
+      size: 30.0,
+    ),
+           /* Icon(
+              
+      CupertinoIcons.bell_fill,
+      color: Colors.green,
+      size: 30.0, ),*/
+            Container(alignment: Alignment.topLeft,
+              child: new CircularPercentIndicator(
+                  radius: 60.0,
+                  lineWidth: 5.0,
+                  percent: 0.5,
+                  center: new Text("50%"),
+                  progressColor: Colors.green,
+                ),),
+           
+            Container(alignment: Alignment.bottomLeft,
+            child:Text('Value : $status', style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+               
+            ),
+           // textAlign: TextAlign.right,
+           )),
+           SizedBox(width: 30.0,),
+           Container(alignment: Alignment.bottomLeft,
+             child:
+             CustomSwitch(
+              activeColor: Colors.pinkAccent,
+              value: status,
+              onChanged: (value) {
+                print("VALUE : $value");
+                setState(() {
+                  status = value;
+                });
+              },
+            )
+           )
+          ],
+        ),
+      ),
+    );
+  }
+}
